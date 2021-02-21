2435da7cd290f39811adca542e2a8992c6b355c5
kishin <78402890+kishinmadhwani@users.noreply.github.com>
2021-02-21 13:04:25 +0530

    Add files via upload

---
+++ codingdevil.py
@@ -0,0 +1,88 @@
+import json
+import discord
+import time
+from discord.ext import commands
+from datetime import date, timedelta
+
+
+with open("setup/Bot_config.json") as file:
+    config = json.load(file)
+
+client = commands.Bot(command_prefix=config["prefix"], id=config["owner_id"])
+client.remove_command("help")
+
+# ----------- functions ---------------
+
+def cur_time():
+    t = time.localtime()
+    n = time.strftime("%I:%H:%S %p",t)
+    return n
+
+def cur_date():
+    today = date.today()
+    return today
+
+def cur_activity(name,mode):
+    if mode == "watching":
+        watching = discord.ActivityType.watching
+        activity = discord.Activity(name=name, type=watching)
+        return activity
+
+    elif mode == "playing":
+        playing = discord.ActivityType.playing
+        activity = discord.Activity(name=name, type=playing)
+        return activity
+
+def cur_guilds():
+    i = len(client.guilds)
+    return i
+
+def version():
+    v = config["version"]
+    return v
+    
+# ----------- Login ---------------
+
+@client.event
+async def on_ready():
+    me = await client.fetch_user(config["id"])
+    print(f"Logged in at {cur_time()}")
+    await me.send(f"``` Client Booted || No Error \n Time: {cur_time()} \n Date: {cur_date()}```")
+    await client.change_presence(activity=cur_activity(f"$help | {cur_guilds()} Guilds","watching"))
+
+# ------------- Cogs ---------------
+
+intial_extensions = [
+                        "cogs.basic"
+                        "cogs.support"
+                        "cogs.error"
+                        "cogs.Mod"
+                    ]
+
+for extension in intial_extensions:
+    client.load_extension(extension)
+    print(f"Loaded: {extension}")
+
+# -------- Manual Cog Control ----------
+@client.command()
+@commands.is_owner()
+async def load(ctx, extension):
+    try:
+        cog = f"cog.{extension}"
+        client.load_extension(extension)
+        await ctx.send(f"Loaded Cog: {extension}")
+    except Exception as e:
+        await ctx.send(f"Error: {e}")
+
+@client.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+    try:
+        cog = f"cog.{extension}"
+        client.unload_extension(extension)
+        await ctx.send(f"Unloaded Cog: {extension}")
+    except Exception as e:
+        await ctx.send(f"Error: {e}")
+
+
+client.run(config["token"])
